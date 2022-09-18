4858030cfb8ac1dacf1d1c2761542d598dad7aab
Arturo Cuya <arturocuya099@gmail.com>
2022-09-18 00:41:06 -0500

    refactor(landing): use tailwindcss for most of landing styles

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,ejs}"],
+    theme: {
+      extend: {},
+      colors: {
+        'white': '#FDFDFD',
+        'black': '#292929',
+        'purple-dark': '#581B98',
+        'purple': '#9C1DE7',
+        'pink': '#F3558E',
+        'alert': '#ADA514'
+      },
+      fontFamily: {
+        
+      }
+    },
+    plugins: [],
+}
