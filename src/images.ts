b073c1f066b398e89c4c0c4e782f4f6284be59d9
Bronley Plumb <bronley@gmail.com>
2022-10-26 09:38:06 -0400

    Fix lint issues

--- src/images.ts
+++ src/images.ts
@@ -1,10 +1,7 @@
-
 export const images = {
     openInNew: {
         src: '/images/icons/open-in-new-pink.svg',
         alt: 'Download Visual Studio Code plugin icon'
     },
-    logo: {
-
-    }
+    logo: {}
 };
--- src/layouts/landing/companies.ts
+++ src/layouts/landing/companies.ts
@@ -1,4 +1,3 @@
-
 export const companies = [
     {
         img: '/images/logos/fubo.png',
