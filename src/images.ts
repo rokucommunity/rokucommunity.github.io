a71893f8fcaf532ec61c99fc2c277eabe14031ba
Bronley Plumb <bronley@gmail.com>
2022-10-26 09:29:46 -0400

    Use local references to all images

---
+++ src/images.ts
@@ -0,0 +1,10 @@
+
+export const images = {
+    openInNew: {
+        src: '/images/icons/open-in-new-pink.svg',
+        alt: 'Download Visual Studio Code plugin icon'
+    },
+    logo: {
+
+    }
+};
---
+++ src/layouts/landing/companies.ts
@@ -0,0 +1,43 @@
+
+export const companies = [
+    {
+        img: '/images/logos/fubo.png',
+        alt: 'Fubo Tv',
+        href: 'https://www.fubo.tv'
+    },
+    {
+        img: '/images/logos/InvertedSolutions.png',
+        alt: 'Inverted Solutions',
+        href: 'mailto:chris@inverted-solutions.com'
+    },
+    {
+        img: '/images/logos/tantawowa.png',
+        alt: 'Tantawowa',
+        href: 'https://tantawowa.com'
+    },
+    {
+        img: '/images/logos/applicaster.png',
+        alt: 'Applicaster',
+        href: 'https://www.applicaster.com'
+    },
+    {
+        img: '/images/logos/REDspace.png',
+        alt: 'REDspace',
+        href: 'https://www.redspace.com'
+    },
+    {
+        img: '/images/logos/corco.png',
+        alt: 'Corco plus',
+        href: 'https://www.miraclechannel.ca/corcoplus'
+    },
+    {
+        img: '/images/logos/phototv.png',
+        alt: 'Photo Tv',
+        href: 'https://channelstore.roku.com/details/222212/phototv'
+    },
+    {
+        img: '/images/logos/haystacknews.png',
+        alt: 'Haystack News',
+        href: 'https://www.haystack.tv'
+    }
+];
